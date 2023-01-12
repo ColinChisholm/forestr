da6362d8ba26c41a74733d696cf6b64e7617b03c
Colin Chisholm <colin.chisholm@unbc.ca>
2023-01-12 23:52:26 +0000

    Initial commit

---
+++ R/fm_collect_basemap.R
@@ -0,0 +1,16 @@
+#' Collect Basemap Data from BC
+#' 
+#' Collects essential transportation and water related layers for an area of 
+#' interest from [BC Data](https://catalogue.data.gov.bc.ca).  This function is 
+#' a wrapper for `bcdata`.
+#' 
+#' @param aoi A `sf` polygon.  
+#' 
+#' 
+#' 
+#' 
+#' 
+
+
+
+fm_collect_basemap
---
+++ R/fm_create_aoi.R
@@ -0,0 +1,24 @@
+#' Create an area of interest 
+#' 
+#' Creates an area of interest interactively.  Simply call the function 
+#' 
+#' @param xmin,xmax longitude minimum and maximum extents (negative for west)
+#' @param ymin,ymax latitude minimum and maximum extents in degrees (negative for south)
+#' @param basemap a Leaflet basemap
+#' 
+#' @return Returns an sf object.  
+#' 
+#' @example 
+#' fm_aoi()
+#' 
+#' 
+#' @export
+
+fm_aoi <- function(xmin = -139, ymin = 49, xmax = -114, ymax = 60,
+                 basemap = "GeoportailFrance.orthos") { ##Other Providers: "CartoDB.Positron", "Esri.WorldImagery", "Esri.DeLorme"
+  d <- leaflet::leaflet() %>%
+    leaflet::fitBounds(xmin, ymin,  xmax, ymax, options = list()) %>%     ## set to extent of BC
+    leaflet::addProviderTiles(basemap) %>%
+    mapedit::editMap()
+  return(d$finished)
+}
