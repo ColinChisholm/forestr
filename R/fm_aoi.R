228deafac6ea5634b53a6d4875a1cccbf67b28be
Colin Chisholm <colin.chisholm@unbc.ca>
2023-01-13 00:24:55 +0000

    Initial commit

--- R/fm_aoi.R
+++ R/fm_aoi.R
@@ -1,17 +1,17 @@
-#' Create an area of interest 
-#' 
-#' Creates an area of interest interactively.  Simply call the function 
-#' 
+#' Create an area of interest
+#'
+#' Creates an area of interest interactively.  Simply call the function
+#'
 #' @param xmin,xmax longitude minimum and maximum extents (negative for west)
 #' @param ymin,ymax latitude minimum and maximum extents in degrees (negative for south)
 #' @param basemap a Leaflet basemap
-#' 
-#' @return Returns an sf object.  
-#' 
-#' @example 
-#' fm_aoi()
-#' 
-#' 
+#'
+#' @return Returns an sf object.
+#'
+#' @examples
+#'aoi <- fm_aoi(xmin = -139, ymin = 49, xmax = -114, ymax = 60)
+#'
+#'
 #' @export
 
 fm_aoi <- function(xmin = -139, ymin = 49, xmax = -114, ymax = 60,
--- R/fm_collect_basemap.R
+++ R/fm_collect_basemap.R
@@ -1,16 +1,25 @@
 #' Collect Basemap Data from BC
-#' 
-#' Collects essential transportation and water related layers for an area of 
-#' interest from [BC Data](https://catalogue.data.gov.bc.ca).  This function is 
+#'
+#' Collects essential transportation and water related layers for an area of
+#' interest from [BC Data](https://catalogue.data.gov.bc.ca).  This function is
 #' a wrapper for `bcdata`.
-#' 
-#' @param aoi A `sf` polygon.  
-#' 
-#' 
-#' 
-#' 
-#' 
+#'
+#' @param aoi A `sf` polygon.
+#' @param outdir A filepath to where the data should be saved. Either relative
+#' to the working directory or an absolute path.
+#' @param type desired spatial data structure.  Geopackage (gpkg) is the default.
+#'
+#' @return Spatial data is saved to the output directory.  A data.frame is returned indicating what data was received.
+#'
+#' @examples#'
+#' fm_collect_basemap(aoi = fm_aoi(), outdir = "./out", type = "gpkg")
+
+#' @export
 
 
+fm_collect_basemap <- function(aoi, outdir = "./out", type = "gpkg") {
 
-fm_collect_basemap
+  layers <- baselayers
+
+  return(layers)
+}
